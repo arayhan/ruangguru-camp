dde1b32550e07b7144c0a449084c6ae69fd3e393
Rayhan <rayhanprima99@gmail.com>
2022-03-22 17:06:36 +0700

    chore: fe - javascript- 3 comparison - script3 - done

--- frontend/javascript/3-comparison-cp/script-3.js
+++ frontend/javascript/3-comparison-cp/script-3.js
@@ -9,3 +9,26 @@
 const score = parseInt(prompt("Masukan nilai: "));
 
 // TODO: answer here
+switch (true) {
+	case score >= 91 && score <= 100:
+		alert("Nilai A");
+		break;
+	case score >= 71 && score <= 90:
+		alert("Nilai B");
+		break;
+	case score >= 61 && score <= 70:
+		alert("Nilai C");
+		break;
+	case score >= 51 && score <= 60:
+		alert("Nilai D");
+		break;
+	case score >= 0 && score <= 50:
+		alert("Nilai E");
+		break;
+	case score < 0 || score > 100:
+		alert("Score hanya dapat diinput dari angka 0 - 100");
+		break;
+	default:
+		alert("Score tidak valid");
+		break;
+}
