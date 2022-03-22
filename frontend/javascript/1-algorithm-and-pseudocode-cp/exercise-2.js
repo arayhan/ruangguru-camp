5d4c075910d201c7afb2b5787119262765eb4dae
Rayhan <rayhanprima99@gmail.com>
2022-03-22 16:42:01 +0700

    chore: fe - js- 1 - exercise2 - done

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
@@ -1,4 +1,15 @@
 // Buatlah pseudocode untuk mengecek apakah seseorang dapat membuat SIM atau tidak.
 // Seseorang diperbolehkan membuat SIM jika usianya lebih besar atau sama dengan 17 tahun.
 
-// TODO: answer here
+// TODO: answer here;
+const prompt = require("prompt");
+
+prompt.start();
+
+const AGE_MINIMUM = 17;
+const successMessage = "BOLEH membuat SIM";
+const failedMessage = "BELUM BOLEH membuat SIM";
+
+prompt.get(["usia"], (err, result) => {
+	console.log(result.usia >= AGE_MINIMUM ? successMessage : failedMessage);
+});
