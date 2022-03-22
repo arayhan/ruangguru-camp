dc85eb8a3c6ae623e41635e64cfaeb7f43eb7000
Rayhan <rayhanprima99@gmail.com>
2022-03-22 16:45:44 +0700

    chore: javascript - 1 - exercise3 - done

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -1,3 +1,13 @@
 // Buatlah pseudocode untuk menjumlahkan bilangan genap yang ada di antara 1 sampai 10
 
 // TODO: answer here
+
+let result = 0;
+
+for (let i = 1; i <= 10; i++) {
+	if (i % 2 === 0) {
+		result += i; // 2 + 4 + 6 + 8 + 10
+	}
+}
+
+console.log(`Result: ${result}`); // 30
