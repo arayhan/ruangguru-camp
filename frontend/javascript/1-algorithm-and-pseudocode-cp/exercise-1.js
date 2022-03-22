9dc7b832da6268adefe84a506202ee1042f6003d
Rayhan <rayhanprima99@gmail.com>
2022-03-22 16:26:07 +0700

    chore: javascript - exercise 1 - done

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-1.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-1.js
@@ -7,6 +7,11 @@
 // 4. Mengumpulkan dan menyerahkan persyaratan
 // 5. Jika persyaratan belum lengkap, maka akan kembali lagi ke proses mengumpulkan persyaratan.
 // 6. Menerima kartu mahasiswa, jas universitas, jadwal orientasi, dan kuliah.
-// 7. Jika kelengkapan dari no. 6 ada yang kurang, lapor ke bagian administrasi, dan kembali menerima ulang kelengkapan tersebut. 
+// 7. Jika kelengkapan dari no. 6 ada yang kurang, lapor ke bagian administrasi, dan kembali menerima ulang kelengkapan tersebut.
 // 7. Mengikuti orientasi.
 // 8. Kuliah berjalan.
+
+// =========================
+// JAWABAN
+// =========================
+// URL : https://miro.com/app/board/uXjVODGE3Xw=/?invite_link_id=173897612621
