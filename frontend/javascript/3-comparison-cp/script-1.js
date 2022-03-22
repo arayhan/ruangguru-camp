a9da670a051b181a68bc8fed2b31532e0f0527e1
Rayhan <rayhanprima99@gmail.com>
2022-03-22 16:54:36 +0700

    chore: fe - javascript - 3 - 2 - done

--- frontend/javascript/3-comparison-cp/script-1.js
+++ frontend/javascript/3-comparison-cp/script-1.js
@@ -3,4 +3,15 @@
 // Masukan suatu angka
 const number = parseInt(prompt("Masukan bilangan: "));
 
+if (number > 0) {
+	alert("Angka positif");
+} else if (number < 0) {
+	alert("Angka negatif");
+} else if (number === 0) {
+	alert("Angka 0");
+} else {
+	alert("Input tidak valid");
+	addEventListener;
+}
+
 // TODO: answer here
