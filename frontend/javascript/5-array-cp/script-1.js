9b5c67bf32b15fbb475a7528477c50981a0746d8
Rayhan <rayhanprima99@gmail.com>
2022-03-23 16:45:26 +0700

    chore: fe - javascript - 5 array cp - done

--- frontend/javascript/5-array-cp/script-1.js
+++ frontend/javascript/5-array-cp/script-1.js
@@ -6,15 +6,25 @@
 // [100, -9, 2, -3, 5] == 100, nilai maksimum bilangan berurutan adalah 100 (bilangan pertama), karena bilangan2 lainnya jika dijumlahkan tidak lebih besar dari 100
 // [1, 2, 3] == 6, nilai maksimum bilangan berurutan adalah 6 (semua bilangan), karena semua bilangan positif dan berurutan, sehingga nilai maksimumnya 6
 // [2, -1, 2, 3, -9] == 6, nilai maksimum bilangan berurutan adalah 6 (penjumlahan dari 2 + (-1) + 2 + 3), lebih besar dari penjumlahan urutan bilangan lainnya
-// [-1, -2, -3] == 0, jika semua bilangan negatif, kita bisa mengembalikan nilai 0 
-
+// [-1, -2, -3] == 0, jika semua bilangan negatif, kita bisa mengembalikan nilai 0
 
 let numInput = prompt("Masukan urutan bilangan dengan pemisah spasi:");
 let arr = numInput.split(" ").map(Number);
 
 // jika tidak ada elemen dalam array, jika semua bilangan negatif, dapat mengembalikan nilai 0
-let maxSum = 0; 
+let maxSum = 0;
 
 // TODO: answer here
+
+for (let i = 0; i < arr.length; i++) {
+	let sum = 0;
+	for (let j = i; j < arr.length; j++) {
+		sum += arr[j];
+		if (sum > maxSum) {
+			console.log(sum);
+			maxSum = sum;
+		}
+	}
+}
 
 console.log(maxSum);
