a96a4356b93e7a300be05a30046b0b69d01424ac
Rayhan <rayhanprima99@gmail.com>
2022-03-23 16:15:39 +0700

    chore: fe - javascript - 4 looping cp - done

--- frontend/javascript/4-looping-cp/script-1.js
+++ frontend/javascript/4-looping-cp/script-1.js
@@ -10,8 +10,20 @@
 //
 // catatam: ingat, 15 merupakan kelipatan 3 dan 5.
 
-
 // Masukan jumlah bilangan yang ingin dicek, iterasi dari angka 1
 const counter = parseInt(prompt("Masukan jumlah bilangan yang ingin dicek: "));
 
 // TODO: answer here
+
+let hasil = "";
+for (let i = 1; i <= counter; i++) {
+	if (i % 3 === 0) {
+		hasil += i + " merupakan kelipatan 3\n";
+	} else if (i % 5 === 0) {
+		hasil += i + " merupakan kelipatan 5\n";
+	} else {
+		hasil += i + "\n";
+	}
+}
+
+console.log(hasil);
