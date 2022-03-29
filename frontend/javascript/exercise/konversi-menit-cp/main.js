8ac1f579271a391c49feeb74e616d254e11a7104
Rayhan <rayhanprima99@gmail.com>
2022-03-29 11:38:43 +0700

    chore: javascript-exercise-konversi-menit-cp - done

--- frontend/javascript/exercise/konversi-menit-cp/main.js
+++ frontend/javascript/exercise/konversi-menit-cp/main.js
@@ -1,18 +1,23 @@
 /**
  * Konversikan menit pada paramter menjadi satuan jam.
- * Misal: 
+ * Misal:
  * - 99 menjadi "1:39"
  * - 132 menjadi "2:12"
- * 
+ *
  * notes:
  * - type data pada parameter akan selalu INTEGER
  * - type data pada result akan selalu berupa STRING
  */
 
-
-
 function konversiMenit(menit) {
-  // TODO: answer here
+	let jam = Math.floor(menit / 60);
+	let menitSisa = menit % 60;
+
+	if (menitSisa < 10) {
+		menitSisa = "0" + menitSisa;
+	}
+
+	return jam + ":" + menitSisa;
 }
 
 console.log(konversiMenit(61));
