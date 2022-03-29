b7fa6d1d397bff57424f0d3064d82d7d3ef227bd
Rayhan <rayhanprima99@gmail.com>
2022-03-29 11:35:41 +0700

    chore: javascript-exercise-sandi-cp - done

--- frontend/javascript/exercise/sandi-cp/main.js
+++ frontend/javascript/exercise/sandi-cp/main.js
@@ -18,13 +18,22 @@ terjemahKataSandi = "PASUKAN 1, MAJU KE AREA 2, JAM 3 SORE"
 Lengkapilah function dengan input kata sandi dan output terjemahannya
 */
 
-
 function terjemahKataSandi(kataSandi) {
-  // TODO: answer here
+	return kataSandi
+		.replace(/&/g, "")
+		.replace(/%/g, "")
+		.replace(/\^/g, "")
+		.replace(/]/g, ",")
+		.replace(/\+/g, "A")
+		.replace(/ /g, "E")
+		.replace(/#/g, " ");
 }
 
-
-console.log(terjemahKataSandi("&P%&+^S&^U&K+%N#1]#M&^+J^%%U#K #+^R&& +#2]#J+M#3#S^%%O&^R #"));
+console.log(
+	terjemahKataSandi(
+		"&P%&+^S&^U&K+%N#1]#M&^+J^%%U#K #+^R&& +#2]#J+M#3#S^%%O&^R #"
+	)
+);
 // PASUKAN 1, MAJU KE AREA 2, JAM 3 SORE
 
-module.exports = terjemahKataSandi
+module.exports = terjemahKataSandi;
