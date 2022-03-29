3ac5ae0e008530c320ec32005989dfd9fbdf3aa2
Rayhan <rayhanprima99@gmail.com>
2022-03-29 09:56:47 +0700

    chore: javascript-exercise-breaking-sentence-cp - done

--- frontend/javascript/exercise/breaking-sentence-cp/main.js
+++ frontend/javascript/exercise/breaking-sentence-cp/main.js
@@ -1,21 +1,24 @@
 /**
- * Kali ini tugas kamu adalah memisahkan setiap kata dalam kalimat. 
+ * Kali ini tugas kamu adalah memisahkan setiap kata dalam kalimat.
  * gunakannlah built-in function dan cari panjang dari setiap kata tersebut.
  */
 
-var word = 'wow JavaScript is so cool and i will become frontend developer';
+var word = "wow JavaScript is so cool and i will become frontend developer";
 var exampleFirstWord = word.substring(0, 3);
 
 // TODO: answer here
+const words = word.split(" ");
 
-console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
-console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength);
-console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWordLength);
-console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
-console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
-console.log('Sixth Word: ' + sixthhWord + ', with length: ' + sixthhWordLength);
-console.log('Seventh Word: ' + seventhWord + ', with length: ' + seventhWordLength);
-console.log('Eighth Word: ' + eighthWord + ', with length: ' + eighthWordLength);
-console.log('Ninth Word: ' + ninthWord + ', with length: ' + ninthWordLength);
-console.log('Tenth Word: ' + tenthWord + ', with length: ' + tenthWordLength);
-console.log('Eleventh Word: ' + eleventhWord + ', with length: ' + eleventhWordLength);
+console.log("First Word: " + words[0] + ", with length: " + words[0].length);
+console.log("Second Word: " + words[1] + ", with length: " + words[1].length);
+console.log("Third Word: " + words[2] + ", with length: " + words[2].length);
+console.log("Fourth Word: " + words[3] + ", with length: " + words[3].length);
+console.log("Fifth Word: " + words[4] + ", with length: " + words[4].length);
+console.log("Sixth Word: " + words[5] + ", with length: " + words[5].length);
+console.log("Seventh Word: " + words[6] + ", with length: " + words[6].length);
+console.log("Eighth Word: " + words[7] + ", with length: " + words[7].length);
+console.log("Ninth Word: " + words[8] + ", with length: " + words[8].length);
+console.log("Tenth Word: " + words[9] + ", with length: " + words[9].length);
+console.log(
+	"Eleventh Word: " + words[10] + ", with length: " + words[10].length
+);
