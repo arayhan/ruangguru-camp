ca66aa7d61cc052c500b938d8bf23eb35335f700
Rayhan <rayhanprima99@gmail.com>
2022-04-04 00:53:31 +0700

    chore: fe - js - hackerrank - birthday-cake-candles - done

--- frontend/javascript/hackerrank/birthday-cake-candles/main.js
+++ frontend/javascript/hackerrank/birthday-cake-candles/main.js
@@ -12,20 +12,33 @@
 // Full Problem: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
 
 function birthdayCakeCandles(candles) {
-  // Write your code here
-	// TODO: answer here
+	if (!candles || !candles.length) return "invalid data";
+
+	candles.sort((a, b) => b - a);
+
+	let max = candles[0];
+	let count = 0;
+
+	for (let i = 0; i < candles.length; i++) {
+		if (candles[i] === max) {
+			count++;
+		} else {
+			break;
+		}
+	}
+
+	return count;
 }
 
 function main() {
-  //var n = parseInt(readLine());
-  //ar = readLine().split(' ');
-  //ar = ar.map(Number);
-  var ar = [3, 2, 1, 3]; // override input
-  var result = birthdayCakeCandles(ar);
-  console.log(result);
-
+	//var n = parseInt(readLine());
+	//arr = readLine().split(' ');
+	//arr = arr.map(Number);
+	var arr = [3, 2, 1, 3]; // override input
+	var result = birthdayCakeCandles(arr);
+	console.log(result);
 }
 
 main(); // execute program
 
-module.exports = birthdayCakeCandles
+module.exports = birthdayCakeCandles;
