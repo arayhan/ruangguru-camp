64b16ef8d4a4523a3487d0b03ef8c4e5628560dd
Rayhan <rayhanprima99@gmail.com>
2022-04-04 01:14:13 +0700

    chore: fe - js - hackerrank - grading-students - done

--- frontend/javascript/hackerrank/gradings-students/main.js
+++ frontend/javascript/hackerrank/gradings-students/main.js
@@ -12,21 +12,39 @@
 // Full Problem: https://www.hackerrank.com/challenges/grading/problem
 
 function gradingStudents(grades) {
-  // Write your code here
-  // TODO: answer here
+	// Write your code here
+	let result = [];
+
+	for (let i = 0; i < grades.length; i++) {
+		let grade = grades[i];
+
+		if (grade < 38) {
+			result.push(grade);
+		} else {
+			let nextMultipleOfFive = grade + (5 - (grade % 5));
+
+			if (nextMultipleOfFive - grade < 3) {
+				result.push(nextMultipleOfFive);
+			} else {
+				result.push(grade);
+			}
+		}
+	}
+
+	return result;
 }
 
 function main() {
-  //     var n = parseInt(readLine());
-  //     var grades = [];
-  //     for(var grades_i = 0; grades_i < n; grades_i++){
-  //        grades[grades_i] = parseInt(readLine());
-  //     }
-  var grades = [73, 67, 38, 33]; // override input
-  var result = gradingStudents(grades);
-  console.log(result.join("\n"));
+	//     var n = parseInt(readLine());
+	//     var grades = [];
+	//     for(var grades_i = 0; grades_i < n; grades_i++){
+	//        grades[grades_i] = parseInt(readLine());
+	//     }
+	var grades = [73, 67, 38, 33]; // override input
+	var result = gradingStudents(grades);
+	console.log(result.join("\n"));
 }
 
 main(); // execute program
 
-module.exports = gradingStudents
+module.exports = gradingStudents;
