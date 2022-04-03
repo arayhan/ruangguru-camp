cc7a760a39041caf751e2d1efb678533b93a1006
Rayhan <rayhanprima99@gmail.com>
2022-04-04 01:31:40 +0700

    chore: fe - js - hackerrank - very-big-sum - done

--- frontend/javascript/hackerrank/very-big-sum/main.js
+++ frontend/javascript/hackerrank/very-big-sum/main.js
@@ -12,16 +12,20 @@
 // Full Problem: https://www.hackerrank.com/challenges/a-very-big-sum/problem
 
 function aVeryBigSum(ar) {
-  // Complete this function
-  // TODO: answer here
+	// Complete this function
+	let sum = 0;
+	for (let i = 0; i < ar.length; i++) {
+		sum += ar[i];
+	}
+	return sum;
 }
 
 function main() {
-  var ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]; // override input
-  var result = aVeryBigSum(ar);
-  console.log(result);
+	var ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]; // override input
+	var result = aVeryBigSum(ar);
+	console.log(result);
 }
 
 main(); // execute program
 
-module.exports = aVeryBigSum
+module.exports = aVeryBigSum;
