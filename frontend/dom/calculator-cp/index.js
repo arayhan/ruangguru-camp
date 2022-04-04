672633db85d9d6a3a8abf431c70595da3ab50850
Rayhan <rayhanprima99@gmail.com>
2022-04-04 10:54:55 +0700

    chore: fe - dom - calculator - done

--- frontend/dom/calculator-cp/index.js
+++ frontend/dom/calculator-cp/index.js
@@ -1,14 +1,41 @@
+const inputEl = document.getElementById("input");
+
 //handle when the number is pressed. It renders the number into #input element
 function number(number) {
-    // TODO: answer here
+	inputEl.value += number;
 }
 
 //handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
 function operator(operator) {
-    // TODO: answer here
+	console.log({ operator });
+	switch (operator) {
+		case "Del":
+			inputEl.value = inputEl.value.slice(0, -1);
+			break;
+		case "AC":
+			inputEl.value = "";
+			break;
+		case "+":
+			inputEl.value += "+";
+			break;
+		case "-":
+			inputEl.value += "-";
+			break;
+		case "*":
+			inputEl.value += "*";
+			break;
+		case "/":
+			inputEl.value += "/";
+			break;
+		case ".":
+			inputEl.value += ".";
+			break;
+		default:
+			break;
+	}
 }
 
 //handle when = button is pressed. It renders the result into #input element
 function calculate() {
-    // TODO: answer here
+	inputEl.value = eval(inputEl.value);
 }
