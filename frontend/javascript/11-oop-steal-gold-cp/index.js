f4879787a3e2aed2428fa78b540b344ed2e9fcf5
Rayhan <rayhanprima99@gmail.com>
2022-04-11 11:15:00 +0700

    chore: cp javascript oop steal gold done

--- frontend/javascript/11-oop-steal-gold-cp/index.js
+++ frontend/javascript/11-oop-steal-gold-cp/index.js
@@ -1,52 +1,64 @@
 class Player {
-  constructor(name) {
-    this.name = name;
-    this.gold = 50;
-    this.stealChance = 0.25;
-    this.hasBeenRobbed = false;
-  }
+	constructor(name) {
+		this.name = name;
+		this.gold = 50;
+		this.stealChance = 0.25;
+		this.hasBeenRobbed = false;
+	}
 
+	getGold() {
+		return this.gold;
+	}
 
-  getGold() {
-  // TODO: answer here
-  }
+	setGold(gold) {
+		this.gold = gold;
+	}
 
-  setGold(gold) {
-  // TODO: answer here
-  }
+	getStealChance() {
+		return this.stealChance;
+	}
 
-  getStealChance() {
-  // TODO: answer here
-  }
+	setStealChance(chance) {
+		this.stealChance = chance;
+	}
 
-  setStealChance(chance) {
-  // TODO: answer here
-  }
+	getHasBeenRobbed() {
+		return this.hasBeenRobbed;
+	}
 
-  getHasBeenRobbed() {
-    return this.hasBeenRobbed;
-  }
-  
-  setHasBeenRobbed(status) {
-    this.hasBeenRobbed = status;
-  }
-  
-  randomizer() {
-    return +Math.random().toFixed(2);
-  }
-  
-  steal(player) {
-    // TODO: answer here
-  }
+	setHasBeenRobbed(status) {
+		this.hasBeenRobbed = status;
+	}
+
+	randomizer() {
+		return Math.random().toFixed(2);
+	}
+
+	steal(player) {
+		if (this.randomizer() < this.getStealChance()) {
+			if (player.getGold() < 5) {
+				return "Lawan terlalu miskin";
+			} else {
+				player.setGold(player.getGold() - 5);
+				this.setGold(this.getGold() + 5);
+				this.setHasBeenRobbed(true);
+				return "Berhasil mencuri 5 gold";
+			}
+		} else if (player.getGold() < 5) {
+			return "Lawan terlalu miskin";
+		}
+
+		return "Gagal mencuri, coba lain kali";
+	}
 }
 
 const p1 = new Player("Fauzan");
 const p2 = new Player("Tegar");
 
-console.log('Jumlah gold Player 1: ' + p1.getGold())
-console.log('Jumlah gold Player 2: ' + p2.getGold())
+console.log("Jumlah gold Player 1: " + p1.getGold());
+console.log("Jumlah gold Player 2: " + p2.getGold());
 console.log(p1.steal(p2));
-console.log('Total gold Player 1: ' + p1.getGold())
-console.log('Total gold Player 2: ' + p2.getGold())
+console.log("Total gold Player 1: " + p1.getGold());
+console.log("Total gold Player 2: " + p2.getGold());
 
 module.exports = Player;
