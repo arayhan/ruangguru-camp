4a880cb6c8abf4b9003258eb09b5ee3e2497aaf2
Rayhan <rayhanprima99@gmail.com>
2022-04-11 11:38:30 +0700

    chore: assignment oop javascript thief-trickster done

--- frontend/javascript/11-oop-thief-and-trickster-assignment/index.js
+++ frontend/javascript/11-oop-thief-and-trickster-assignment/index.js
@@ -1,47 +1,65 @@
 const Player = require("../11-oop-steal-gold-cp/index");
 
 class Thief extends Player {
-  constructor(name) {
-    super(name);
-    this.job = "Thief";
-  }
-  
-  robbedBlind() {
-    // TODO: answer here
-  }
+	constructor(name) {
+		super(name);
+		this.job = "Thief";
+	}
 
-  steal(player) {
-    // TODO: answer here
-  }
+	robbedBlind() {
+		this.stealChance = 0.75;
+
+		return this.gold < 10 ? "Aku terlalu miskin" : this.setStealChance;
+	}
+
+	steal(player) {
+		if (player.randomizer() > player.getDistractionPurseChance()) {
+			return "Gagal mencuri balik";
+		} else {
+			player.setGold(player.getGold() - 5);
+			this.setGold(this.getGold() + 5);
+
+			if (player.getDistractionPurseChance() > this.randomizer()) {
+				if (this.getGold() < 10) {
+					player.setGold(player.getGold() + this.getGold());
+					this.setGold(0);
+					return "Berhasil mencuri balik semua uang lawan";
+				}
+
+				player.setGold(player.getGold() + 10);
+				this.setGold(this.getGold() - 10);
+				return "Berhasil mencuri balik 10 gold";
+			}
+		}
+	}
 }
 
 class Trickster extends Player {
-  constructor(name) {
-    super(name);
-    this.distractionPurseChance = 0.25;
-    this.job = "Trickster";
-  }
+	constructor(name) {
+		super(name);
+		this.distractionPurseChance = 0.25;
+		this.job = "Trickster";
+	}
 
-  setDistractionPurseChance(chance) {
-    // TODO: answer here
-  }
+	setDistractionPurseChance(chance) {
+		this.distractionPurseChance = chance;
+	}
 
-  getDistractionPurseChance() {
-    // TODO: answer here
-  }
+	getDistractionPurseChance() {
+		return this.distractionPurseChance;
+	}
 
-  distractionPurse(player) {
-    const rng = this.randomizer();
-    // TODO: answer here
-  }
+	distractionPurse(player) {
+		const rng = this.randomizer();
+	}
 
-  steal(player) {
-    // TODO: answer here
-  }
+	steal(player) {
+		// TODO: answer here
+	}
 }
 
 // Dilarang menghapus code dibawah ini!!!
 module.exports = {
-  Thief,
-  Trickster,
+	Thief,
+	Trickster,
 };
