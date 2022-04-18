d98f8c1725c67520d50116b1d1a2c8268e9aeab5
Rayhan <rayhanprima99@gmail.com>
2022-04-18 17:29:06 +0700

    chroe: es6 - 5-destructuring-cp - done

--- frontend/es6/5-destructuring-cp/index.js
+++ frontend/es6/5-destructuring-cp/index.js
@@ -1,9 +1,10 @@
 // format nama user menjadi "John - john@example.com", dan akses nama user dengan teknik destrucuring assignment.
 
 const format = (user) => {
-  // TODO: answer here
+	const { name, email } = user;
+	return `${name} - ${email}`;
 };
 
-console.log(format({ name: "John", email: "john@example.com" }))
+console.log(format({ name: "John", email: "john@example.com" }));
 
-module.exports = format
+module.exports = format;
