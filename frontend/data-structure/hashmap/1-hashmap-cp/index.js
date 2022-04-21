06c61026c1bca19012db79ed12fc68e051064132
Rayhan <rayhanprima99@gmail.com>
2022-04-21 22:15:30 +0700

    chore: frontend\data-structure\hashmap\1-hashmap-cp done

--- frontend/data-structure/hashmap/1-hashmap-cp/index.js
+++ frontend/data-structure/hashmap/1-hashmap-cp/index.js
@@ -1,26 +1,43 @@
 function ageDistribution(persons) {
-    let result = new Map();
-    // TODO: answer here
-    return result
+	let result = new Map();
+
+	people.forEach((person) => {
+		if (result.has(person.age)) {
+			result.set(person.age, result.get(person.age) + 1);
+		} else {
+			result.set(person.age, 1);
+		}
+	});
+
+	return result;
 }
 
 function groupByAge(persons) {
-    let result = new Map();
-    // TODO: answer here
-    return result
+	let result = new Map();
+
+	people.forEach((person) => {
+		let age = person.age;
+		if (!result.has(age)) {
+			result.set(age, []);
+		}
+		result.get(age).push(person);
+	});
+
+	return result;
 }
 
 let people = [
-    { name: "Bob", age: 21 },
-    { name: "Sam", age: 28 },
-    { name: "Ann", age: 21 },
-    { name: "Joe", age: 22 },
-    { name: "Ben", age: 28 },
-]
+	{ name: "Bob", age: 21 },
+	{ name: "Sam", age: 28 },
+	{ name: "Ann", age: 21 },
+	{ name: "Joe", age: 22 },
+	{ name: "Ben", age: 28 },
+];
 
 console.log(ageDistribution(people));
 console.log(groupByAge(people));
 
 module.exports = {
-    ageDistribution, groupByAge
-}
+	ageDistribution,
+	groupByAge,
+};
