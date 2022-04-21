7106321e76b72506ee50f460a4a7a3aa87db9fed
Rayhan <rayhanprima99@gmail.com>
2022-04-22 00:43:55 +0700

    chore: frontend\data-structure\assignment\scoreboard done

--- frontend/data-structure/assignment/scoreboard/index.js
+++ frontend/data-structure/assignment/scoreboard/index.js
@@ -1,20 +1,42 @@
 class Score {
-    constructor(name, correct, wrong, empty) {
-        // TODO: answer here
-    }
+	constructor(name, correct, wrong, empty) {
+		this.name = name;
+		this.correct = correct;
+		this.wrong = wrong;
+		this.empty = empty;
+	}
 }
 
 class Scores {
-    constructor(scores) {
-        // TODO: answer here
-    }
+	constructor(scores) {
+		this.scores = scores;
+	}
 
-    topStudents() {
-        // TODO: answer here
-    }
+	topStudents() {
+		const totalScores = this.scores.map(({ name, correct, wrong }) => {
+			const correctScore = correct * 4;
+			const wrongScore = wrong * -1;
+			const totalScore = correctScore + wrongScore;
+
+			return { name, correct, wrong, totalScore };
+		});
+
+		totalScores.sort((a, b) => {
+			console.log(a, b);
+			if (a.totalScore === b.totalScore) {
+				if (a.correct === b.correct) return a.name.localeCompare(b.name);
+				return b.correct - a.correct;
+			}
+
+			return b.totalScore - a.totalScore;
+		});
+
+		// return totalScores;
+		return totalScores.map((score) => score.name);
+	}
 }
 
 module.exports = {
-    Score,
-    Scores
-}
+	Score,
+	Scores,
+};
