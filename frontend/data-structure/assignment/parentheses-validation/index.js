c91c4a2cb3996f9ca8f712c6b14d6670b4a92d57
Rayhan <rayhanprima99@gmail.com>
2022-04-21 23:49:15 +0700

    chore: frontend\data-structure\assignment\parentheses-validation done

--- frontend/data-structure/assignment/parentheses-validation/index.js
+++ frontend/data-structure/assignment/parentheses-validation/index.js
@@ -1,9 +1,25 @@
-const Stack = require('./stack')
+const Stack = require("./stack");
 
 function IsValidParentheses(s) {
-    // TODO: answer here
+	if (s.length === 0) return false;
+
+	let stack = new Stack();
+
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+			stack.push(s[i]);
+		} else if (s[i] === ")" || s[i] === "]" || s[i] === "}") {
+			if (stack.data.length === 0) return false;
+			let top = stack.pop();
+			if (top === "(" && s[i] !== ")") return false;
+			if (top === "[" && s[i] !== "]") return false;
+			if (top === "{" && s[i] !== "}") return false;
+		}
+	}
+
+	return stack.data.length === 0;
 }
 
 module.exports = {
-    IsValidParentheses
-}
+	IsValidParentheses,
+};
