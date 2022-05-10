52a431f22e6d73cb12e2d6bd9f6c5d14193d8da7
Rayhan <rayhanprima99@gmail.com>
2022-05-10 08:52:53 +0700

    chore (state-props-component-cp): done

--- frontend/react/state-props-in-component/state-props-component-cp/src/App.js
+++ frontend/react/state-props-in-component/state-props-component-cp/src/App.js
@@ -1,77 +1,114 @@
-import React from 'react';
+import React from "react";
 
-export const Item = (props) => {
-  // TODO: answer here
-  const [item, setItem] = React.useState(0);
+export const Item = ({ id, image, title, showAlert, onChangeTotal }) => {
+	const [item, setItem] = React.useState(0);
 
-  return(
-    <div className='each-box'>
-      <img src={image} alt="each-item"/>
-      <p>{title}</p>
-      <div>
-        <button
-        aria-label={`minus-button-${id}`}
-        onClick={()=>{
-        // TODO: answer here
-        }}
-        >-</button>
-        <input type="text" aria-label={`item-${id}`} className={`item-${id}`} value={item} disabled></input>
-        <button
-        aria-label={`add-button-${id}`}
-        onClick={()=>{
-          // TODO: answer here
-        }}
-        >+</button>
-      </div>
-    </div>
-  )
-}
-
+	return (
+		<div className="each-box">
+			<img src={image} alt="each-item" />
+			<p>{title}</p>
+			<div>
+				<button
+					aria-label={`minus-button-${id}`}
+					onClick={() => {
+						if (item > 0) {
+							setItem(item - 1);
+							onChangeTotal("MINUS");
+						}
+					}}
+				>
+					-
+				</button>
+				<input
+					type="text"
+					aria-label={`item-${id}`}
+					className={`item-${id}`}
+					value={item}
+					disabled
+				></input>
+				<button
+					aria-label={`add-button-${id}`}
+					onClick={() => {
+						if (item < 10) {
+							setItem(item + 1);
+							onChangeTotal("ADD");
+						} else showAlert(true);
+					}}
+				>
+					+
+				</button>
+			</div>
+		</div>
+	);
+};
 
 function App() {
-  //Add state for total 
-  // TODO: answer here
- 
-  const dataDummy = [
-    {
-      id    : '1',
-      image : "https://picsum.photos/id/0/150/",
-      title : "Laptop",
-    },
-    {
-      id    : '2',
-      image : "https://picsum.photos/id/1010/150/",
-      title : "Buku",
-    },
-    {
-      id    : '3',
-      image : "https://picsum.photos/id/103/150/",
-      title : "Sepatu",
-    }
-  ];
+	const [itemsAmount, setItemsAmount] = React.useState([]);
+	const [total, setTotal] = React.useState(0);
 
-  const showAlert = (isShow) => {
-    if(isShow){
-      alert("Ups, udah kelebihan yaa");
-    }
-  }
+	const dataDummy = [
+		{
+			id: "1",
+			image: "https://picsum.photos/id/0/150/",
+			title: "Laptop",
+		},
+		{
+			id: "2",
+			image: "https://picsum.photos/id/1010/150/",
+			title: "Buku",
+		},
+		{
+			id: "3",
+			image: "https://picsum.photos/id/103/150/",
+			title: "Sepatu",
+		},
+	];
 
-  return (
-    <div className="state-props-2" aria-label="AppRoot">
-      <h3>Keranjang Belanja</h3>
-      <div className='box-container'>
-          {dataDummy.map((element, index) => (
-            // TODO: answer here
-          ))}
-      </div>
-      <div className='end-section'>
-          <h4>Kamu udah masukin&nbsp;
-            <span className='total-item' aria-label="total-item">{total}
-            </span>&nbsp;
-          barang</h4>
-      </div>
-    </div>
-  );
+	const showAlert = (isShow) => {
+		if (isShow) {
+			alert("Ups, udah kelebihan yaa");
+		}
+	};
+
+	const handleChangeTotal = (TYPE) => {
+		switch (TYPE) {
+			case "ADD":
+				setTotal(total + 1);
+				break;
+			case "MINUS":
+				setTotal(total - 1);
+				break;
+			default:
+				break;
+		}
+	};
+
+	return (
+		<div className="state-props-2" aria-label="AppRoot">
+			<h3>Keranjang Belanja</h3>
+			<div className="box-container">
+				{dataDummy.map((element) => (
+					<Item
+						key={element.id}
+						id={element.id}
+						image={element.image}
+						title={element.title}
+						showAlert={showAlert}
+						onChangeTotal={handleChangeTotal}
+					/>
+				))}
+			</div>
+			<div className="end-section">
+				<h4>
+					Kamu udah masukin&nbsp;
+					<span className="total-item" aria-label="total-item">
+						{total}
+					</span>
+					&nbsp; barang
+				</h4>
+			</div>
+		</div>
+	);
 }
 
 export default App;
