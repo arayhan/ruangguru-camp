97021fc803c38ccef77fb1bd2a35fe73619a8dea
Rayhan <rayhanprima99@gmail.com>
2022-05-11 10:26:44 +0700

    chore (react-handle-events-cp): done

--- frontend/react/react-handle-events/react-handle-events-cp/src/Screens/PodcastLists.js
+++ frontend/react/react-handle-events/react-handle-events-cp/src/Screens/PodcastLists.js
@@ -8,45 +8,59 @@ import PodcastListItem from "../components/PodcastListItem";
 import Constants from "../Constants";
 
 const PodcastLists = () => {
-  const [podcastList, setPodcastList] = useState([]);
-  const [showFormModal, setShowFormModal] = useState(false);
-  const [formModalType, setFormModalType] = useState("ADD");
-  const [podcastId, setPodcastId] = useState(0)
+	const [podcastList, setPodcastList] = useState([]);
+	const [showFormModal, setShowFormModal] = useState(false);
+	const [formModalType, setFormModalType] = useState("ADD");
+	const [podcastId, setPodcastId] = useState(0);
 
-  const getPodcastListData = async () => {
-    // TODO: answer here
-  };
+	const getPodcastListData = async () => {
+		const response = await axios.get(Constants.API_URL);
+		setPodcastList(response.data);
+	};
 
-  useEffect(() => {
-    // TODO: answer here
-  }, []);
+	useEffect(() => {
+		getPodcastListData();
+	}, []);
 
-  return (
-    <div>
-      <Header headerText={"My Top Podcasts"} />
-      <div>
-        <Button
-          style={{ marginTop: 10, marginLeft: 10, alignSelf: "flex-end" }}
-          variant="primary"
-          onClick={() => setShowFormModal(true)}
-        >
-          + Add Podcasts
-        </Button>
-        {podcastList.map((item) => {
-          // TODO: answer here
-        })}
-      </div>
-      <PodcastFormModal
-        podcastList={podcastList}
-        setPodcastList={setPodcastList}
-        showFormModal={showFormModal}
-        setShowFormModal={setShowFormModal}
-        setFormModalType={setFormModalType}
-        formModalType={formModalType}
-        podcastId={podcastId}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<Header headerText={"My Top Podcasts"} />
+			<div>
+				<Button
+					style={{ marginTop: 10, marginLeft: 10, alignSelf: "flex-end" }}
+					variant="primary"
+					onClick={() => {
+						setShowFormModal(true);
+						setFormModalType("ADD");
+					}}
+				>
+					+ Add Podcasts
+				</Button>
+				{podcastList.map((item) => {
+					return (
+						<PodcastListItem
+							key={item.id}
+							id={item.id}
+							podcastItem={item}
+							setPodcastId={(id) => setPodcastId(id)}
+							setFormModalType={(type) => setFormModalType(type)}
+							setShowFormModal={(show) => setShowFormModal(show)}
+						/>
+					);
+				})}
+			</div>
+			<PodcastFormModal
+				podcastList={podcastList}
+				setPodcastList={setPodcastList}
+				showFormModal={showFormModal}
+				setShowFormModal={setShowFormModal}
+				setFormModalType={setFormModalType}
+				formModalType={formModalType}
+				podcastId={podcastId}
+				onSuccess={getPodcastListData}
+			/>
+		</div>
+	);
 };
 
 export default PodcastLists;
