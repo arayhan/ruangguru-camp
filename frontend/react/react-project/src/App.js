fb1f1ee4c06a0f472b1f56899e2c35b8ebc52b36
Rayhan <rayhanprima99@gmail.com>
2022-05-23 22:13:18 +0700

    chore (Navbar): add Navbar to App & add browser router on index

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,18 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
 	return (
 		<div aria-label="App">
-			<header className="shadow-md">
-				<div className="container">
-					<div className="py-5">
-						<div
-							className="font-logo text-xl font-semibold"
-							aria-label="App Title"
-						>
-							Instagram Clone
-						</div>
-					</div>
-				</div>
-			</header>
+			<Navbar />
 		</div>
 	);
 }
--- frontend/react/react-project/src/components/Navbar.js
+++ frontend/react/react-project/src/components/Navbar.js
@@ -1,10 +1,22 @@
-import { useEffect, useState } from "react"
-import React from "react"
-
-import { getSession, auth } from "../api/auth"
-import { useContext } from "react"
-import { SessionContext } from "../context/SessionContext"
+import { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { getSession, auth } from "../api/auth";
+import { SessionContext } from "../context/SessionContext";
 
 export default function Navbar() {
-  // TODO: answer here
+  return (
+    <header className="shadow-md" aria-label="Navbar">
+      <div className="container">
+        <div className="py-5">
+          <Link
+            className="font-logo text-xl font-semibold"
+            to="/"
+            aria-label="App Title"
+          >
+            Instagram Clone
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
