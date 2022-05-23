ce71faf5ef110e2c03d1ada0bd0d6b88122ff407
Rayhan <rayhanprima99@gmail.com>
2022-05-23 23:05:48 +0700

    chore (Navbar): add logo image

--- frontend/react/react-project/src/components/Navbar.js
+++ frontend/react/react-project/src/components/Navbar.js
@@ -2,21 +2,33 @@ import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { getSession, auth } from "../api/auth";
 import { SessionContext } from "../context/SessionContext";
+import Logo from "../images/instagram-logo.png";
 
 export default function Navbar() {
+  const context = useContext(SessionContext);
+
   return (
-    <header className="shadow-md" aria-label="Navbar">
+    <div className="shadow-md bg-white" aria-label="Navbar">
       <div className="container">
         <div className="py-5">
-          <Link
-            className="font-logo text-xl font-semibold"
-            to="/"
-            aria-label="App Title"
-          >
-            Instagram Clone
+          <Link className="inline-flex items-center space-x-3" to="/">
+            <span>
+              <img
+                className="w-6"
+                src={Logo}
+                alt="Instagram Logo"
+                aria-label="App Logo"
+              />
+            </span>
+            <span
+              className="font-logo text-xl font-semibold"
+              aria-label="App Title"
+            >
+              Instagram Clone
+            </span>
           </Link>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
