a25d477341395132349e26a9a63cfb1deae9e34e
Rayhan <rayhanprima99@gmail.com>
2022-05-23 22:00:38 +0700

    chore: add header

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,7 +1,18 @@
 function App() {
 	return (
 		<div aria-label="App">
-			<div aria-label="App Title">Instagram Clone</div>
+			<header className="shadow-md">
+				<div className="container">
+					<div className="py-5">
+						<div
+							className="font-logo text-xl font-semibold"
+							aria-label="App Title"
+						>
+							Instagram Clone
+						</div>
+					</div>
+				</div>
+			</header>
 		</div>
 	);
 }
