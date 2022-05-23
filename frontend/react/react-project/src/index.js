c87f009e2b4f3d28a4deb87f21e6601dc043510c
Rayhan <rayhanprima99@gmail.com>
2022-05-24 00:36:44 +0700

    chore (SessionProvider): move session provider to index

--- frontend/react/react-project/src/index.js
+++ frontend/react/react-project/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { SessionProvider } from "./context/SessionContext";
 
 import "./styles/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <SessionProvider>
+        <App />
+      </SessionProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
