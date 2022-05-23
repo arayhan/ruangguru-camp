2a659809a54271db0bc43ee6ef8c5e9f0a6caab2
Rayhan <rayhanprima99@gmail.com>
2022-05-24 01:52:45 +0700

    chore (SessionContext): add default value

--- frontend/react/react-project/src/context/SessionContext.js
+++ frontend/react/react-project/src/context/SessionContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
-  const [session, setSession] = useState();
+  const [session, setSession] = useState(null);
 
   return (
     <SessionContext.Provider value={{ session, setSession }}>
