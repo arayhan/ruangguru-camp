d8fe91a1255905acfecb33afbc9e37f9c6ae7940
Rayhan <rayhanprima99@gmail.com>
2022-05-25 00:53:17 +0700

    chore (PostList): update responsive grid cols layout

--- frontend/react/react-project/src/routes/PostList.js
+++ frontend/react/react-project/src/routes/PostList.js
@@ -22,7 +22,7 @@ function PostList() {
                 {({ posts }) => {
                   console.log({ posts });
                   return (
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                       {posts &&
                         posts.map((post) => (
                           <CardImage
