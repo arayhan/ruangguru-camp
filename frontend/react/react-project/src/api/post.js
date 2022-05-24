fddc458d5db6bde15de8c158d7740cf1792dd219
Rayhan <rayhanprima99@gmail.com>
2022-05-25 02:04:04 +0700

    chore (post api): change post api from promise to async await

--- frontend/react/react-project/src/api/post.js
+++ frontend/react/react-project/src/api/post.js
@@ -1,31 +1,27 @@
 import axios from 'axios';
 import { API_URL } from './config';
 
-const getPosts = (callback) => {
+const getPosts = async (callback) => {
   const config = { withCredentials: true };
 
-  axios
-    .get(API_URL + '/post/list', config)
-    .then((response) => {
-      callback(response.data.data);
-    })
-    .catch((error) => {
-      console.log('getPosts Err : ', error);
-    });
+  try {
+    const response = await axios.get(API_URL + '/post/list', config);
+    callback(response.data.data);
+  } catch (error) {
+    console.log('getPosts Err : ', { error });
+  }
 };
 
-const postLikeDislike = (id, data, callback) => {
+const postLikeDislike = async (id, data, callback) => {
   const { type } = data;
   const config = { withCredentials: true };
 
-  axios
-    .post(API_URL + `/post/${id}/${type}`, null, config)
-    .then((response) => {
-      callback(response.data);
-    })
-    .catch((error) => {
-      console.log('getPosts Err : ', error);
-    });
+  try {
+    const response = await axios.post(API_URL + `/post/${id}/${type}`, null, config);
+    callback(response.data.data);
+  } catch (error) {
+    console.log('postLikeDislike Err : ', { error });
+  }
 };
 
 export { getPosts, postLikeDislike };
