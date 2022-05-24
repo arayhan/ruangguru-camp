1f24bdd9daa438e80a4f508e8a4adc9f49399ef9
Rayhan <rayhanprima99@gmail.com>
2022-05-25 02:03:39 +0700

    chore (LikeDislikeButton): adjust action function with unit test

--- frontend/react/react-project/src/components/LikeDislikeButton.js
+++ frontend/react/react-project/src/components/LikeDislikeButton.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi';
 import { postLikeDislike } from '../api/post';
 import { usePost } from '../context/PostContext';
@@ -5,27 +6,69 @@ import { usePost } from '../context/PostContext';
 export default function LikeDislikeButton({ postId, isLiked, isDisliked, likeCount, dislikeCount }) {
   const handleGetPosts = usePost().handleGetPosts;
 
+  const [likeCountState, setLikeCountState] = useState(likeCount);
+  const [dislikeCountState, setDislikeCountState] = useState(dislikeCount);
+  const [isLikedState, setIsLikedState] = useState(isLiked);
+  const [isDislikedState, setIsDislikedState] = useState(isDisliked);
+
   const likeActiveClass = 'bg-primary text-white';
   const likeInactiveClass = 'text-primary hover:bg-primary hover:bg-opacity-10';
   const dislikeActiveClass = 'bg-danger text-white';
   const dislikeInactiveClass = 'text-danger hover:bg-danger hover:bg-opacity-10';
 
   const handleClickLikeDislike = (type) => {
-    postLikeDislike(postId, { type }, () => handleGetPosts());
+    if (type === LikeDislikeButton.TYPES.LIKE) {
+      if (isLikedState) {
+        setLikeCountState(likeCountState - 1);
+        setIsLikedState(false);
+      } else {
+        setLikeCountState(likeCountState + 1);
+        setIsLikedState(true);
+      }
+
+      if (isDislikedState) {
+        setDislikeCountState(dislikeCountState - 1);
+        setIsDislikedState(false);
+      }
+    } else {
+      if (isDislikedState) {
+        setDislikeCountState(dislikeCountState - 1);
+        setIsDislikedState(false);
+      } else {
+        setDislikeCountState(dislikeCountState + 1);
+        setIsDislikedState(true);
+      }
+
+      if (isLikedState) {
+        setLikeCountState(likeCountState - 1);
+        setIsLikedState(false);
+      }
+    }
+
+    // postLikeDislike(postId, { type }, () => handleGetPosts());
   };
+
+  useEffect(() => {
+    console.log({
+      likeCountState,
+      dislikeCountState,
+      isLikedState,
+      isDislikedState,
+    });
+  }, [likeCountState, dislikeCountState, isLikedState, isDislikedState]);
 
   return (
     <div className="flex items-center justify-end space-x-3">
       <div className="flex items-center space-x-2">
         <button
           className={`flex items-center space-x-2 transition-all border border-primary px-3 py-2 rounded-md ${
-            isLiked ? likeActiveClass : likeInactiveClass
+            isLikedState ? likeActiveClass : likeInactiveClass
           }`}
           onClick={() => handleClickLikeDislike(LikeDislikeButton.TYPES.LIKE)}
           aria-label="Like Button"
         >
           <span className="text-xs" aria-label="Like Count">
-            {likeCount}
+            {likeCountState}
           </span>
           <FiThumbsUp />
         </button>
@@ -34,13 +77,13 @@ export default function LikeDislikeButton({ postId, isLiked, isDisliked, likeCou
       <div className="flex items-center space-x-2">
         <button
           className={`flex items-center space-x-2 transition-all border border-danger px-3 py-2 rounded-md ${
-            isDisliked ? dislikeActiveClass : dislikeInactiveClass
+            isDislikedState ? dislikeActiveClass : dislikeInactiveClass
           }`}
           onClick={() => handleClickLikeDislike(LikeDislikeButton.TYPES.DISLIKE)}
           aria-label="Dislike Button"
         >
           <span className="text-xs" aria-label="Dislike Count">
-            {dislikeCount}
+            {dislikeCountState}
           </span>
           <FiThumbsDown />
         </button>
@@ -52,6 +95,4 @@ export default function LikeDislikeButton({ postId, isLiked, isDisliked, likeCou
 LikeDislikeButton.TYPES = {
   LIKE: 'like',
   DISLIKE: 'dislike',
-  UNLIKE: 'unlike',
-  UNDISLIKE: 'undislike',
 };
