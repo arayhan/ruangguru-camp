2b53667c8d61f96ee0218a46a0b2eae073c583cf
Rayhan <rayhanprima99@gmail.com>
2022-05-25 02:08:26 +0700

    chore (App): remove LikeDislikeButton component after testing

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -2,14 +2,12 @@
 import { SessionProvider } from './context/SessionContext';
 import Navbar from './components/Navbar';
 import PostList from './routes/PostList';
-import LikeDislikeButton from './components/LikeDislikeButton';
 // import UploadForm from './components/UploadForm';
 
 function App() {
   return (
     <SessionProvider>
       <div aria-label="App">
-        <LikeDislikeButton likeCount={0} dislikeCount={289} isLiked={false} isDisliked={false} />
         <Navbar />
         <PostList />
         {/* <Routes>
