c45bdc8cd85d1c09b77d8ec9697180452d805ac9
Rayhan <rayhanprima99@gmail.com>
2022-05-24 13:21:00 +0700

    chore (Router): move browser router to index

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import PostList from './routes/PostList';
 // import UploadForm from './components/UploadForm';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="bg-gray-200" aria-label="App">
-        <Navbar />
-        <Routes>
-          {/* <Route index element={} /> */}
-          <Route index path="/" element={<PostList />} />
-          {/* <Route path="upload" element={<UploadForm />} /> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div aria-label="App">
+      <Navbar />
+      <Routes>
+        {/* <Route index element={} /> */}
+        <Route index path="/" element={<PostList />} />
+        {/* <Route path="upload" element={<UploadForm />} /> */}
+      </Routes>
+    </div>
   );
 }
 
