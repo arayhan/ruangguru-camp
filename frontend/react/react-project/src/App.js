c4b902aeade2db9998d368d378f58c83d0d85e87
Rayhan <rayhanprima99@gmail.com>
2022-05-25 02:05:49 +0700

    chore (App): comment route component for a while

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import { SessionProvider } from './context/SessionContext';
 import Navbar from './components/Navbar';
 import PostList from './routes/PostList';
+import LikeDislikeButton from './components/LikeDislikeButton';
 // import UploadForm from './components/UploadForm';
 
 function App() {
   return (
     <SessionProvider>
       <div aria-label="App">
+        <LikeDislikeButton likeCount={0} dislikeCount={289} isLiked={false} isDisliked={false} />
         <Navbar />
-        <Routes>
-          {/* <Route index element={} /> */}
-          <Route index path="/" element={<PostList />} />
-          {/* <Route path="upload" element={<UploadForm />} /> */}
-        </Routes>
+        <PostList />
+        {/* <Routes>
+        <Route index element={} />
+        <Route index path="/" element={} />
+        <Route path="upload" element={<UploadForm />} />
+        </Routes> */}
       </div>
     </SessionProvider>
   );
